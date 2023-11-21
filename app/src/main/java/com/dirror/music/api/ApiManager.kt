84c6e9000025f3349cc238f78845fa0bdf332b20
haha <ministarmail@msn.cn>
2023-11-21 12:06:09 +0800

    修复混乱的API定义

--- app/src/main/java/com/dirror/music/api/ApiManager.kt
+++ app/src/main/java/com/dirror/music/api/ApiManager.kt
@@ -1,5 +1,12 @@
 package com.dirror.music.api
+import com.dirror.music.manager.User
 
+/**
+ * 网易云官方 api
+ */
+const val API_NETEASE = "https://music.163.com/api"
+
+const val API_FROMUSER = User.neteaseCloudMusicApi
 
 /**
  * Dso Music 默认 API
@@ -9,41 +16,15 @@ const val API_DEFAULT = "https://cloudmusic.moriafly.xyz"
 /**
  * Dso Music 备用接口
  */
-const val API_DSO = "https://netease-cloud-music-api-lemon.vercel.app"
 
-/**
- * @名称 音乐 网易云 api
- * @限制 暂无
- * @测试 主要 api，绝大多数稳定
- * @类型 适用 NodeJs 版
- */
-const val API_MUSIC_API = "http://musicapi.leanapp.cn"
+const val API_DSO = API_FROMUSER
+const val API_MUSIC_API = API_FROMUSER
+const val API_FCZBL_VIP = API_FROMUSER
+const val API_IMJAD = API_FROMUSER
 
-/**
- * @名称 网易云简单 api
- * @限制 暂无，很快，稳定
- * @测试 用于歌词，很简单
- * @获取歌词 https://api.fczbl.vip/163/?type=lrc&id=1438470159 // 传入歌曲 id
- *
- */
-const val API_FCZBL_VIP = "https://api.fczbl.vip/163"
+const val API_MUSIC_ELEUU = API_FROMUSER
 
-
-// https://api.imjad.cn/cloudmusic/?type=song&id=545505696&br=999000
-const val API_IMJAD = "https://api.imjad.cn/cloudmusic"
-
-/**
- * 网易云官方 api
- */
-const val API_NETEASE = "https://music.163.com/api"
-
-/**
- * @类型 适用 NodeJs 版
- * @例子 http://music.eleuu.com/banner?type=1
- */
-const val API_MUSIC_ELEUU = "http://music.eleuu.com"
-
-const val API_AUTU = "https://autumnfish.cn"
+const val API_AUTU = API_FROMUSER
 
 const val API_LOGIN = API_AUTU
 
