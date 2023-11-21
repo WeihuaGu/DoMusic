b23ddce5a98c32b3a5623f6d2721299db401d7f1
haha <ministarmail@msn.cn>
2023-11-21 13:40:03 +0800

    oo

--- app/src/main/java/com/dirror/music/api/ApiManager.kt
+++ app/src/main/java/com/dirror/music/api/ApiManager.kt
@@ -1,13 +1,9 @@
 package com.dirror.music.api
-import com.dirror.music.manager.User
 
 /**
  * 网易云官方 api
  */
 const val API_NETEASE = "https://music.163.com/api"
-
-@JvmField
-const val API_FROMUSER = User.neteaseCloudMusicApi
 
 /**
  * Dso Music 默认 API
@@ -18,22 +14,15 @@ const val API_DEFAULT = "https://cloudmusic.moriafly.xyz"
  * Dso Music 备用接口
  */
 
-@JvmField
-const val API_DSO = API_FROMUSER
-@JvmField
-const val API_MUSIC_API = API_FROMUSER
-@JvmField
-const val API_FCZBL_VIP = API_FROMUSER
-@JvmField
-const val API_IMJAD = API_FROMUSER
+const val API_DSO = API_DEFAULT
+const val API_MUSIC_API = API_DEFAULT
+const val API_FCZBL_VIP = API_DEFAULT
+const val API_IMJAD = API_DEFAULT
 
-@JvmField
-const val API_MUSIC_ELEUU = API_FROMUSER
+const val API_MUSIC_ELEUU = API_DEFAULT
 
-@JvmField
-const val API_AUTU = API_FROMUSER
+const val API_AUTU = API_DEFAULT
 
-@JvmField
 const val API_LOGIN = API_AUTU
 
 
